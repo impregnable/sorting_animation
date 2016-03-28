2e84111f58c0d16de4f1b6602dd4afa442d37290
impregnable <ImpregnableFiend@gmail.com>
2016-03-28 16:28:27 +0300

    html page with btn dat can swap 2 elements

--- script.js
+++ script.js
@@ -1,46 +1,72 @@
+$(document).ready(function() {
+
 // creating random array
 
-function createRandomDigit(start, end) {
+function createRandomNumber(start, end) {
   return Math.random() * (end - start) + start;
 }
 
 function createRandomArray(length, start, end) {
   var array = [];
   for ( var i = 0; i < length; i++) {
-    array.push(Math.ceil(createRandomDigit(start, end)));
+    array.push(Math.ceil(createRandomNumber(start, end)));
   }
   return array;
 }
 
-// reflect dat array to user
+// swap
 
-$(document).ready(function() {
-  $('button').click(function() {
+function swap(array, i1, i2) {
+    var a = array[i1];
+    array[i1] = array[i2];
+    array[i2] = a;
+}
+
+// render array
+
+  $('.first').click(function() {
     var array = createRandomArray(10, 0, 10);
-    var content = array.map(function(el) {
+    var arrayView = array.map(function(el, index) {
       return $('<span>')
         .text(el)
-        .css( {
-          'border':'2px solid #990066',
-          'padding':'15px',
-          'margin': '5px'
-        });
+        .addClass('style-each-el')
+        .css({ left: index * 40 });
     });
-    $('#array-placeholder').html(content);
+    $('#array-placeholder').html(arrayView);
+
+    // swapView
+
+    function swapView(array, arrayView, i, j) {
+        swap(array, i, j);
+
+        var a = arrayView[i].animate({ left: j * 40}).promise();
+          console.log(arrayView[i].offset());
+        var b = arrayView[j].animate({ left: i * 40}).promise();
+          console.log(arrayView[j].offset());
+      return  $.when(a, b).then(function() {
+          swap(arrayView, i, j);
+        })
+    }
+
+    swapView(array, arrayView, 0, 1).then(function() { alert('Done!') }, 1000);
+    });
+
+
   });
-});
+
+
 
 // sorting dat array
 
-function bubbleSort(array) {
-  for ( var i = 0; i < array.length-1; i++) {
-    for ( var j = 0; j < array.length-1-i; j++) {
-      if ( array[j+1] < array[j]) {
-        var t = array[j+1];
-        array[j+1] = array[j];
-        array[j] = t;
-      }
-    }
-  }
-  return array;
-}
+// function bubbleSort(array) {
+//   for ( var i = 0; i < array.length-1; i++) {
+//     for ( var j = 0; j < array.length-1-i; j++) {
+//       if ( array[j+1] < array[j]) {
+//         var t = array[j+1];
+//         array[j+1] = array[j];
+//         array[j] = t;
+//       }
+//     }
+//   }
+//   return array;
+// }
