ad8bf32bdc6e2f1433508ad13dfad7ac82db8c9c
impregnable <ImpregnableFiend@gmail.com>
2016-03-29 20:20:27 +0300

    last technical commit

--- script.js
+++ script.js
@@ -2,29 +2,44 @@ $(document).ready(function() {
 
 // creating random array
 
-function createRandomNumber(start, end) {
-  return Math.random() * (end - start) + start;
-}
-
-function createRandomArray(length, start, end) {
-  var array = [];
-  for ( var i = 0; i < length; i++) {
-    array.push(Math.ceil(createRandomNumber(start, end)));
+  function createRandomNumber(start, end) {
+    return Math.random() * (end - start) + start;
   }
-  return array;
-}
+
+  function createRandomArray(length, start, end) {
+    var array = [];
+    for ( var i = 0; i < length; i++) {
+      array.push(Math.ceil(createRandomNumber(start, end)));
+    }
+    return array;
+  }
 
 // swap
 
-function swap(array, i1, i2) {
-    var a = array[i1];
-    array[i1] = array[i2];
-    array[i2] = a;
-}
+  function swap(array, i1, i2) {
+      var a = array[i1];
+      array[i1] = array[i2];
+      array[i2] = a;
+  }
+
+// swapView
+
+  function swapView(array, arrayView, i, j) {
+    swap(array, i, j);
+
+    var a = arrayView[i].animate({ left: j * 40}).promise();
+
+    var b = arrayView[j].animate({ left: i * 40}).promise();
+
+    return $.when(a, b).then(function() {
+      swap(arrayView, i, j);
+    });
+  }
 
 // render array
 
   $('.first').click(function() {
+
     var array = createRandomArray(10, 0, 10);
     var arrayView = array.map(function(el, index) {
       return $('<span>')
@@ -34,39 +49,24 @@ function swap(array, i1, i2) {
     });
     $('#array-placeholder').html(arrayView);
 
-    // swapView
+    var nextStep = function(i, j) {
+      function next_i_j() {
+        if ( j <= array.length-2-i) { j += 1 }
+        else {
+           j = 0; i +=1
+        }
+      }
 
-    function swapView(array, arrayView, i, j) {
-        swap(array, i, j);
-
-        var a = arrayView[i].animate({ left: j * 40}).promise();
-          console.log(arrayView[i].offset());
-        var b = arrayView[j].animate({ left: i * 40}).promise();
-          console.log(arrayView[j].offset());
-      return  $.when(a, b).then(function() {
-          swap(arrayView, i, j);
-        })
+      if ( i >= array.length) { return; }
+      
+      if ( array[j+1] < array[j]) {
+        swapView(array, arrayView, j+1, j).then(function() {
+          next_i_j(); nextStep(i, j)
+        });
+      } else {
+        next_i_j(); nextStep(i, j);
+      }
     }
-
-    swapView(array, arrayView, 0, 1).then(function() { alert('Done!') }, 1000);
-    });
-
-
+    nextStep(0, 0);
   });
-
-
-
-// sorting dat array
-
-// function bubbleSort(array) {
-//   for ( var i = 0; i < array.length-1; i++) {
-//     for ( var j = 0; j < array.length-1-i; j++) {
-//       if ( array[j+1] < array[j]) {
-//         var t = array[j+1];
-//         array[j+1] = array[j];
-//         array[j] = t;
-//       }
-//     }
-//   }
-//   return array;
-// }
+});
